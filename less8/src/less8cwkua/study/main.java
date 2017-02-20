1a757656ba7e01afb777e1058ffed510a31c99b3
Nazar <nazarrrk0@gmail.com>
2017-02-20 19:46:28 +0200

    first commit

---
+++ less8/src/less8cwkua/study/main.java
@@ -0,0 +1,43 @@
+package less8cwkua.study;
+
+public class main {
+
+	public static void main(String[] args) {
+	//	// TODO Auto-generated method stub
+		//                       0     1      2        3
+		String [] names = {"roman","sona","hamlo","romanov"};
+		
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
+		}
+		System.out.println();
+		for(String n: names){ 
+			System.out.println(n);
+			
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
